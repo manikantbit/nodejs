9d22ff8d5fdec6857198cdaaa92748e5a0ac547f
manikantbit <manikant_bit@rediffmail.com>
2018-02-17 20:47:35 -0800

    .

--- app.js
+++ app.js
@@ -56,9 +56,9 @@ var db_name = "test" ;
 
 var db_host = (process.env.mongodb_host || "ds239988.mlab.com" ) ;
 var db_port = (process.env.mongodb_port || "39988" ) ;
-var db_user = (process.env.mongodb_user || "cmpe281" ) ;
+var db_user = (process.env.mongodb_user || "heroku_tb3nblzw" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
-var db_name = (process.env.mongodb_name || "test" ) ;
+var db_name = (process.env.mongodb_name || "heroku_tb3nblzw" ) ;
    
 
 var db = new DB(db_name,
